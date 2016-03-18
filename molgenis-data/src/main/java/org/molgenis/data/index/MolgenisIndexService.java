4fd401d47ee4ec9b6b8532b6953fa27b92f4c41d
jjettenn <jjettenn@gmail.com>
2016-03-18 15:15:15 +0100

    Step 1 make Elasticsearch Utils available in data package

---
+++ molgenis-data/src/main/java/org/molgenis/data/index/MolgenisIndexService.java
@@ -0,0 +1,9 @@
+package org.molgenis.data.index;
+
+
+public interface MolgenisIndexService
+{
+	public static final String DEFAULT_INDEX_NAME = "molgenis";
+
+	public MolgenisIndexUtil getMolgenisIndexUtil();
+}
